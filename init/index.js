cdde6fc1628149d631a4ed8de7475b4b6c3d7d96
Nibha840 <nibha6470@gmail.com>
2025-04-17 14:11:13 +0530

    your update message

--- init/index.js
+++ init/index.js
@@ -5,7 +5,6 @@ const Listing = require("../models/listing.js");
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
-
 main()
     .then(() => {
         console.log("connected to DB");
@@ -22,7 +21,7 @@ const initDB = async () => {
     await Listing.deleteMany({});
     initdata.data = initdata.data.map((obj) => ({
         ...obj,
-        owner: "5a9427648b0beebeb69579e7",
+        owner: "5a9427648b0beebeb6957a22",
     }));
     await Listing.insertMany(initdata.data);
     console.log("data was initialized");
